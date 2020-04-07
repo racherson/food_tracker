d036aaa974416de13968e82166874416a573894c
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2020-04-07 15:15:19 -0500

    add table view cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Rachel Anderson on 4/7/20.
+//  Copyright © 2020 Rachel Anderson. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
