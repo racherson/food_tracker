f2187f94bad1a209067470034bd0688d72ece79a
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2020-04-07 12:39:27 -0500

    add rating control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,13 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Rachel Anderson on 4/7/20.
+//  Copyright © 2020 Rachel Anderson. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+}
